bf1e1c0051ef330a87f2e7316d73b4ee4e098167
shindoo <trofim-dima06@mail.ru>
2025-04-11 04:21:55 +0700

    Update 4

--- cat/events_app/models.py
+++ cat/events_app/models.py
@@ -1,10 +1,24 @@
 from django.db import models
 from django.template.defaultfilters import slugify
 from random import randint
-
+import datetime
 from django.urls import reverse
 from unidecode import unidecode
 
+russian_months = [
+    "января",  # 1
+    "февраля",  # 2
+    "марта",  # 3
+    "апреля",  # 4
+    "мая",  # 5
+    "июня",  # 6
+    "июля",  # 7
+    "августа",  # 8
+    "сентября",  # 9
+    "октября",  # 10
+    "ноября",  # 11
+    "декабря"  # 12
+]
 class Events(models.Model):
     id = models.IntegerField(primary_key=True)
     idfull = models.CharField(max_length=20)
@@ -26,13 +40,36 @@ class Events(models.Model):
     places = models.TextField()
     slug_name = models.CharField(max_length=200, blank=True)
 
+
     def save_slug(self, *args, **kwargs):
         name = unidecode(self.name)
         self.slug_name = slugify(name) + '_' + str(randint(1, 100000))
         super().save(*args, **kwargs)
 
+    def time_start(self):
+        date_obj = datetime.datetime.fromtimestamp(self.date_start)
+        day = date_obj.day
+        month = russian_months[date_obj.month - 1]  # Индекс на 1 меньше номера месяца
+        year = date_obj.year
+        hour = date_obj.hour
+        minute = date_obj.minute
+        formatted_date = f"{day} {month} {year} в {hour:02d}:{minute:02d}"
+        return formatted_date
+
+    def time_end(self):
+        date_obj = datetime.datetime.fromtimestamp(self.date_end)
+        day = date_obj.day
+        month = russian_months[date_obj.month - 1]  # Индекс на 1 меньше номера месяца
+        year = date_obj.year
+        hour = date_obj.hour
+        minute = date_obj.minute
+        formatted_date = f"{day} {month} {year} в {hour:02d}:{minute:02d}"
+        return formatted_date
+
     def __str__(self):
         return self.name
+
+
 
 
 class Profiles(models.Model):
@@ -59,4 +96,4 @@ class Profiles(models.Model):
 
 
 
-# Create your models here.
+
